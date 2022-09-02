36e30c1321694897a5ae3f4cfca442701cbba78a
sachincb <sachincb08@gmail.com>
2022-09-02 19:08:44 +0530

    Update Main.java

--- bunchbycycles/Main.java
+++ bunchbycycles/Main.java
@@ -2,19 +2,33 @@ package bunchbycycles;
 
 public class Main {
   static class CountsByUsage {
-    public int lowCount = 0;
-    public int mediumCount = 0;
-    public int highCount = 0;
+    public static int lowCount = 0;
+    public static int mediumCount = 0;
+    public static int highCount = 0;
   };
 
   static CountsByUsage countBatteriesByUsage(int[] cycles) {
     CountsByUsage counts = new CountsByUsage();
+     //traversing through the input array
+	    for(int i=0;i<cycles.length;i++) {
+	    	//counting for battery having less than 400 charge cycle
+	    	if(cycles[i]<400) {
+	    		counts.lowCount++;
+	    	}
+	    	//counting for battery having between 400 and 919 charge cycle
+	    	else if(cycles[i]>=400 && cycles[i]<=919) {
+	    		counts.mediumCount++;
+	    	}
+	    	else if(cycles[i]>=920) {
+	    		counts.highCount++;
+	    	}
+	    }
     return counts;
   }
 
   static void testBucketingByNumberOfCycles() {
     System.out.println("Counting batteries by usage cycles...\n");
-    CountsByUsage counts = countBatteriesByUsage(new int[] {100, 300, 500, 600, 900, 1000});
+    CountsByUsage counts = countBatteriesByUsage(new int[] {100, 300, 500, 600, 900, 1000,919,});
     assert(counts.lowCount == 2);
     assert(counts.mediumCount == 3);
     assert(counts.highCount == 1);
